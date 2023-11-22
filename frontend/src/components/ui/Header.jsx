22763b5895e8934abc1f8773b99a3c8f17793ec7
Probe1st <svintsitskiy.leha@mail.ru>
2023-11-22 18:31:36 +0500

    создание хедера и подключение шрифтов

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -2,14 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex flex-row justify-evenly h-20 border">
-      <Link to={"/"}>Home</Link>
-      <Link to={"auth/"}>auth</Link>
-      <Link to={"comands/"}>comands</Link>
-      <Link to={"comands/1/"}>comands</Link>
-      <Link to={"tournaments/"}>tournaments</Link>
-      <Link to={"tournaments/1/"}>tournament</Link>
-      <Link to={"tournaments/:id/settings/"}>TournamentsSettings</Link>
-    </div>
+    <header className="flex flex-row justify-between items-center mb-10 font-semibold">
+      {/* logo */}
+      <div className="w-12 h-12 bg-red-600"></div>
+
+      {/* nav */}
+      <nav className="flex flex-row justify-between space-x-7">
+        <Link to={"/tournaments"}>Турниры</Link>
+        <Link to={'/comands'}>Команды</Link>
+        
+      </nav>
+
+      {/* sign in link */}
+      <div>
+        <Link className="bg-[#921515] px-6 py-2 rounded-full" to={"/auth"}>Войти</Link>
+      </div>
+    </header>
   )
 }
