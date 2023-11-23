467d3042f82de19eb4ce7420020c1f168b964fe3
Egor Saratov <sekboyego111@gmail.com>
2023-11-23 22:54:11 +0500

    upd strapi2

---
+++ backend/my-project/src/api/user-sport/controllers/user-sport.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-sport controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-sport.user-sport');
---
+++ backend/my-project/src/api/user-sport/services/user-sport.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-sport service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::user-sport.user-sport');
